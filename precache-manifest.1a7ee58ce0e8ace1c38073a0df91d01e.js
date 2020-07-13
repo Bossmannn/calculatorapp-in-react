a6ec7c5bf4c23b5265581844fd5c3960e7c0551e
MJ329 <durowadeabdulmajeed@gmail.com>
2020-07-13 02:16:18 -0600

    Updates

---
+++ precache-manifest.1a7ee58ce0e8ace1c38073a0df91d01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6778e4f556b9a6fce069a2c7960f8f07",
+    "url": "/calculatorapp-in-react/index.html"
+  },
+  {
+    "revision": "33447f93dcd05b23885e",
+    "url": "/calculatorapp-in-react/static/css/main.16102918.chunk.css"
+  },
+  {
+    "revision": "db6e8e3e500b2a0b6a01",
+    "url": "/calculatorapp-in-react/static/js/2.4b4f1af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculatorapp-in-react/static/js/2.4b4f1af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33447f93dcd05b23885e",
+    "url": "/calculatorapp-in-react/static/js/main.6a3892f3.chunk.js"
+  },
+  {
+    "revision": "946b61f12afeda4fb8c7",
+    "url": "/calculatorapp-in-react/static/js/runtime-main.54befbd6.js"
+  }
+]);
